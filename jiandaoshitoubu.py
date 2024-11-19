65bf0543c0cff75de2c2328b0577a28d03d81ab5
Scluzlep <hengshan-kkk@outlook.com>
2024-11-19 16:46:27 +0800

    202411191646

---
+++ jiandaoshitoubu.py
@@ -0,0 +1,24 @@
+import random as rd
+
+while True:
+    s = input("要进行游戏吗？（是/否）")
+    if s == '是':
+        break
+    elif s == '否':
+        print("再见！")
+    else:
+        print("输入有误，请重新输入！")
+        continue
+
+print("游戏开始！")
+player_choice = input("请输入您的选择：")
+computer_choice = rd.choice(['剪刀','石头','布'])
+
+if player_choice == computer_choice:
+    print(f"电脑出的是{computer_choice},你出的是{player_choice},平局")
+elif (player_choice == '剪刀' and computer_choice == '布') \
+        or (player_choice == '石头' and computer_choice == '剪刀') \
+        or (player_choice == '布' and computer_choice == '石头'):
+    print(f"电脑出的是{computer_choice},你出的是{player_choice},恭喜你赢了")
+else:
+    print(f"电脑出的是{computer_choice},你出的是{player_choice},很遗憾，你输了")
