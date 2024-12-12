9eef0a1aa525795b95c5cb367dc9ad5e2f1f850f
Scluzlep <hengshan-kkk@outlook.com>
2024-12-12 16:48:18 +0800

    202412121648

---
+++ matplot6.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import numpy as np
+# import xlrd
+
+plt.rcParams['font.sans-serif'] = 'SimHei'
+
+class1_score = pd.read_excel('学生成绩表.xls')
+class2_score = pd.read_excel('211班成绩表.xls')
+class1 = [class1_score['Python程序设计'].mean(), class1_score['数据库'].mean(), class1_score['数据结构'].mean(),
+          class1_score['数据处理'].mean(),]
+class2 = [class2_score['Python程序设计'].mean(), class2_score['数据库'].mean(), class2_score['数据结构'].mean(),
+          class2_score['数据处理'].mean(),]
+
+width = 0.35
+x_index = range(len(class1))
+plt.bar([i - width / 2 for i in x_index], class1, color='red', label='一班', alpha=0.6, width=width)
+plt.bar([i + width / 2 for i in x_index], class2, color='blue', label='二班', alpha=0.6, width=width)
+plt.legend(loc='upper left')
+plt.title('学生成绩')
+plt.xlabel('科目')
+plt.ylabel('成绩')
+plt.xticks(x_index, ['Python程序设计', '数据库', '数据结构', '数据处理'])
+plt.show()
--- matplt5.py
+++ matplt5.py
@@ -1,10 +1,22 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import numpy as np
 # import xlrd
 
-if __name__ == '__main__':
-    data = pd.read_excel('score.xls')
-    print(data)
-    ls = data.values.tolist()
-    print(ls)
+plt.rcParams['font.sans-serif'] = 'SimHei'
+data = pd.read_excel('学生成绩表.xls')
+print(data)
+name = data['姓名']
+score1 = data['Python程序设计']
+score2 = data['数据库']
+score3 = data['数据结构']
+score4 = data['数据处理']
+plt.plot(name, score1, color='red', marker='o', linestyle='-', label='Python程序设计')
+plt.plot(name, score2, color='blue', marker=',', linestyle='-', label='数据库')
+plt.plot(name, score3, color='green', marker='<', linestyle='-', label='数据结构')
+plt.plot(name, score4, color='black', marker='.', linestyle='-', label='数据处理')
+plt.legend(loc='upper left')
+plt.title('学生成绩')
+plt.xlabel('姓名')
+plt.ylabel('成绩')
+plt.show()
